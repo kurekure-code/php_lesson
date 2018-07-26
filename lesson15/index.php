d8fe119eac78e570d936e67241e4917b87e75758
hoge <s03281@mcpc-0215.cyberagent.co.jp>
2018-07-26 14:37:43 +0900

    lesson15

---
+++ lesson15/index.php
@@ -0,0 +1,14 @@
+<?php
+    $count = 1;
+    // while($count <= 10){
+    //     echo "Hello" . "($count)" . "<br>";
+    //     $count++;
+    // }
+
+    do{
+        echo $count."<br>";
+        $count++;
+    }while($count <= 10);
+
+
+?>
