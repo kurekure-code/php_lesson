5a84e16b19592303818e11c075fc290e30301754
hoge <s03281@mcpc-0215.cyberagent.co.jp>
2018-07-26 16:11:21 +0900

    lesson18

---
+++ lesson18/index.php
@@ -0,0 +1,16 @@
+<?php
+    // echo "php" . "<br>";
+
+    // exit();
+    // exit('exit');
+    // die();
+    // die("err");
+
+    // print "exit";
+
+
+    mysqli_connect("localhost", "root", "rot") or die("DB connect err");
+
+    echo "Connect!!!!!!!!!!!!!!!!!!!";
+
+ ?>
