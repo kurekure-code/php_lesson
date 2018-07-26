a4b56291bd4597abf530a5dc625c56da596a62e6
hoge <s03281@mcpc-0215.cyberagent.co.jp>
2018-07-26 13:26:52 +0900

    lesson10

---
+++ lesson10/index.php
@@ -0,0 +1,12 @@
+<?php
+    $day = "Thursday";
+    $date = 32;
+    $year = 1202;
+
+
+    echo "The date is" . " " . "<strong>" . $day . " " . $date . " " . $year . "</strong>" . "." . "<br>";
+
+    echo "This is $day $date $year" . "<br>";
+
+    echo 'This is $day $date $year' . "<br>";
+ ?>
