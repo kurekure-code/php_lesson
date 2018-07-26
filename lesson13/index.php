8037c1f16b82d5856cb22eb229b273c1079e0d53
hoge <s03281@mcpc-0215.cyberagent.co.jp>
2018-07-26 14:28:45 +0900

    lesson12+14

---
+++ lesson13/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $num1 = 12;
+    $num2 = 32;
+    $num3 = 10;
+
+    // echo $num1 + $num2 + $num3;
+
+    // echo $num1 + ($num2 - $num3);
+
+    echo ($num1 + $num2 + $num3) * 2;
+
+ ?>
---
+++ lesson14/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $number1 = 12;
+    $number2 = 2;
+    $number3 = 3;
+
+    if( ($number1 - $number2 == 10) && ($number2 + $number3) == 5){
+        if( $number1 + $number3 == 15){
+            echo "15!";
+        }else{
+            echo "soso,";
+        }
+    }else{
+        echo "NG";
+    }
+
+ ?>
