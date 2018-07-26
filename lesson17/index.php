0ca5febd0e08882c824e5194ca23d15d9b622e4e
hoge <s03281@mcpc-0215.cyberagent.co.jp>
2018-07-26 15:59:30 +0900

    lesson16+17

---
+++ lesson17/index.php
@@ -0,0 +1,40 @@
+<?php
+
+    $number = "";
+
+    // switch($number){
+    //     case 1:
+    //         echo "One";
+    //         break;
+    //     case 2:
+    //         echo "II";
+    //         break;
+    //     case 3:
+    //         echo '<a href="">3</a>';
+    //         break;
+    //     default:
+    //         echo "?";
+    //         break;
+    // }
+
+
+    switch($number){
+        case 1:
+        // case "":
+            echo "One";
+            break;
+        case 2:
+        case "":
+            echo "II";
+            break;
+        case 3:
+            echo '<a href="">3</a>';
+            break;
+        default:
+            echo "?";
+            break;
+    }
+
+
+
+?>
