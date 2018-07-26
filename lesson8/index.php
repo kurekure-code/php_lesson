ff73f72a910d57437d7f922ccb80f3ac5bada432
hoge <s03281@mcpc-0215.cyberagent.co.jp>
2018-07-26 13:17:35 +0900

    lesson8

---
+++ lesson8/index.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "Hello World";
+
+ ?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>lesson8</title>
+    </head>
+    <body>
+        <input type="text" name="" value="<?php echo $name; ?>">
+    </body>
+</html>
